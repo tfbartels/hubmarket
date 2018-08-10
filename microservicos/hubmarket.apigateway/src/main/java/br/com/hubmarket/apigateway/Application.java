b9c247844f64a579510a391efee26f7aadd63a10
Thiago Bartels <tfbartels@gmail.com>
2018-08-10 17:19:04 -0300

    Commit inicial microservicos

---
+++ microservicos/hubmarket.apigateway/src/main/java/br/com/hubmarket/apigateway/Application.java
@@ -0,0 +1,14 @@
+package br.com.hubmarket.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
