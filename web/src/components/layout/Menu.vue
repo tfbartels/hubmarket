fd6f9dd202374c5c9fa9eefdd8945c42a337d345
Thiago Bartels <tfbartels@gmail.com>
2018-08-10 17:27:33 -0300

    Commit inicial cliente web

---
+++ web/src/components/layout/Menu.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-menu :default-active="activeIndex" class="menu" mode="horizontal" @select="handleSelect">
+    <el-submenu index="1">
+      <template slot="title">Menu 1</template>
+      <el-menu-item index="1-1">item one</el-menu-item>
+      <el-menu-item index="1-2">item two</el-menu-item>
+      <el-menu-item index="1-3">item three</el-menu-item>
+      <el-submenu index="1-4">
+        <template slot="title">item four</template>
+        <el-menu-item index="1-4-1">item one</el-menu-item>
+        <el-menu-item index="1-4-2">item two</el-menu-item>
+        <el-menu-item index="1-4-3">item three</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-submenu index="2">
+      <template slot="title">Menu 2</template>
+      <el-menu-item index="2-1">item one</el-menu-item>
+      <el-menu-item index="2-2">item two</el-menu-item>
+      <el-menu-item index="2-3">item three</el-menu-item>
+      <el-submenu index="2-4">
+        <template slot="title">item four</template>
+        <el-menu-item index="2-4-1">item one</el-menu-item>
+        <el-menu-item index="2-4-2">item two</el-menu-item>
+        <el-menu-item index="2-4-3">item three</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'Menu'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
---
+++ web/src/components/view/Destaque.vue
@@ -0,0 +1,31 @@
+<template>
+ <div>
+  <el-row>
+    <el-col :span="8" v-for="(o) in 12" :key="o">
+      <el-card shadow="hover" >
+        <img src="../../assets/logo.png" class="image">
+        <div id="desc-prod">Descricao do produto</div>
+        <div>Preço</div>
+      </el-card>
+    </el-col>
+  </el-row>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Destaque'
+}
+</script>
+
+<style>
+  .el-card {
+    margin:10%;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+</style>
