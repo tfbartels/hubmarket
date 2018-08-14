7e559716b901d61b885c5dfad9b318455193b0d3
Thiago Bartels <tfbartels@gmail.com>
2018-08-13 23:08:03 -0300

    Desenvolvimento microservico de seguranca

---
+++ microservicos/hubmarket.usuario/src/main/java/br/com/hubmarket/usuario/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.hubmarket.usuario;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Microserviço de usuario";
+    }
+}
