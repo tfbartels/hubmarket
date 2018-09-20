740995286d83f4d81fff48cbbd8f6b18f641070b
Thiago Bartels <tfbartels@gmail.com>
2018-09-20 20:46:38 -0300

    Adicionando arquivo dados Usuario

---
+++ microservicos/hubmarket.usuario/dataUsuario.sql
@@ -0,0 +1,23 @@
+INSERT INTO public.usuario(id, email, nome)
+	VALUES (1, 'contato@pontofrio.com', 'Ponto Frio');
+	
+INSERT INTO public.fornecedor(cnpf, razao_social, id)
+	VALUES ('67043082000122', 'Ponto Frio Ltda', 1);
+
+INSERT INTO public.usuario(id, email, nome)
+	VALUES (2, 'contato@casasbahia.com', 'Casas Bahia');
+	
+INSERT INTO public.fornecedor(cnpf, razao_social, id)
+	VALUES ('82888138000133', 'Casas Bahia Ltda', 2);
+	
+INSERT INTO public.usuario(id, email, nome)
+	VALUES (3, 'contato@mazineluiza.com', 'Magazine Luiza');
+	
+INSERT INTO public.fornecedor(cnpf, razao_social, id)
+	VALUES ('96211818000122', 'Magazine Luiza Ltda', 3);	
+
+INSERT INTO public.usuario(id, email, nome)
+	VALUES (4, 'contato@ricardoeletro.com', 'Ricardo Eletro');
+	
+INSERT INTO public.fornecedor(cnpf, razao_social, id)
+	VALUES ('86343979000116', 'Ricardo Eletro Ltda', 4);	
